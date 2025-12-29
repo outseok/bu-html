10a5e7e452c82c23619ad5a14e80264e30934278
ouseok <dlstjr6454@gmail.com>
2025-12-29 13:21:55 +0900

    php

--- exam.php
+++ exam.php
@@ -4,14 +4,26 @@
     </head>
     <body>
         <?php
-        $weight = 60;
-        $height = 170;
-        $result = ($height - 100) * 0.9;
-        if ($weight > $result) {
-            echo ("다이어트 필요");
+            $i = 1;
+            $j = 2;
+        while ($j<= 9){
+        
+            while ($i <= 9){
+            
+                 $r = $j * $i;
+                echo "$j x $i = $r <br>";
+                $i++;
         }
-        else {
-            echo ("정상 체중");
+        $j++;
+        $i = 1;
+
+        }
+
+        for ($j =2; $j <= 9; $j++) {
+            for ($i = 1; $i <= 9; $i++) {
+                $r = $j * $i;
+                echo "$j x $i = $r <br>";
+            }
         }
         ?>
     </body>
